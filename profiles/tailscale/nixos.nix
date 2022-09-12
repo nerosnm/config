6007bae2642cf53ebc5d4013ac72225aa43659a2
Søren Mortensen <soren@neros.dev>
2022-09-12 13:18:12 +0100

    Turn off some tailscale things (like autoconnect)

--- profiles/tailscale/nixos.nix
+++ profiles/tailscale/nixos.nix
@@ -7,31 +7,31 @@
 {
   services.tailscale.enable = true;
 
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to tailscale";
-
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig.Type = "oneshot";
-
-    script = ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        echo "already authenticated, doing nothing"
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      echo "authenticating..."
-      ${pkgs.tailscale}/bin/tailscale up -authkey "file:/run/agenix/tailscale-authkey"
-    '';
-  };
+  # systemd.services.tailscale-autoconnect = {
+  #   description = "Automatic connection to tailscale";
+  #
+  #   after = [ "network-pre.target" "tailscale.service" ];
+  #   wants = [ "network-pre.target" "tailscale.service" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #
+  #   serviceConfig.Type = "oneshot";
+  #
+  #   script = ''
+  #     # wait for tailscaled to settle
+  #     sleep 2
+  #
+  #     # check if we are already authenticated to tailscale
+  #     status="$(${pkgs.tailscale}/bin/tailscale status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
+  #     if [ $status = "Running" ]; then # if so, then do nothing
+  #       echo "already authenticated, doing nothing"
+  #       exit 0
+  #     fi
+  #
+  #     # otherwise authenticate with tailscale
+  #     echo "authenticating..."
+  #     ${pkgs.tailscale}/bin/tailscale up -authkey "file:/run/agenix/tailscale-authkey"
+  #   '';
+  # };
 
   networking.firewall = {
     # Allow traffic from my Tailscale network
@@ -45,6 +45,6 @@
     checkReversePath = "loose";
   };
 
-  networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
-  networking.search = [ "example.com.beta.tailscale.net" ];
+  # networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
+  # networking.search = [ "example.com.beta.tailscale.net" ];
 }
