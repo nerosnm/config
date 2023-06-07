a6b35f1d6ab16ae05ea22d58ef492efa4805f23d
Søren Mortensen <soren@neros.dev>
2023-06-07 11:00:21 +0100

    home(nvim): move git blame info to lualine

--- home/modules/nvim/config/lualine.lua
+++ home/modules/nvim/config/lualine.lua
@@ -1,6 +1,7 @@
 vim.opt.showmode = false
 
 local onehalf = require'lualine.themes.onedark'
+local git_blame = require'gitblame'
 
 local colors = {
     fg = '#dcdfe4',
@@ -8,6 +9,33 @@ local colors = {
 
 onehalf.normal.b.fg = colors.fg
 onehalf.normal.c.fg = colors.fg
+
+function blame_components()
+    local components = {}
+
+    if not git_blame.is_blame_text_available() then
+        return components
+    end
+
+    -- Annoyingly, gitblame returns true for is_blame_text_available() when in
+    -- an empty buffer.
+    if vim.bo.filetype == '' then
+        return components
+    end
+
+    local blame = git_blame.get_current_blame_text()
+    for str in string.gmatch(blame, "([^•]+)") do
+        table.insert(components, (string.gsub(str, "^%s*(.-)%s*$", "%1")))
+    end
+
+    return components
+end
+
+function blame_component(idx)
+    local components = blame_components()
+    local component = components[idx]
+    return component
+end
 
 require'lualine'.setup {
     options = {
@@ -20,6 +48,7 @@ require'lualine'.setup {
             left = '',
             right = '',
         },
+        globalstatus = true,
     },
     sections = {
         lualine_a = {
@@ -42,10 +71,30 @@ require'lualine'.setup {
                     hint = '⚙ ',
                 },
             },
-            --             {
-            --                 treelocation,
-            --                 cond = treelocation_available,
-            --             }
+            -- {
+            --     blame_components,
+            --     cond = git_blame.is_blame_text_available,
+            -- }
+            {
+                function() return blame_component(1) end,
+                cond = function() return blame_component(1) ~= nil end,
+                -- color = { fg = '#7c828c' },
+            },
+            {
+                function() return blame_component(2) end,
+                cond = function() return blame_component(2) ~= nil end,
+                -- color = { fg = '#7c828c' },
+            },
+            {
+                function() return blame_component(3) end,
+                cond = function() return blame_component(3) ~= nil end,
+                -- color = { fg = '#7c828c' },
+            },
+            {
+                function() return blame_component(4) end,
+                cond = function() return blame_component(4) ~= nil end,
+                -- color = { fg = '#7c828c' },
+            },
         },
         lualine_x = {
             'encoding',
